6adeedd2dd8022b49acc7ce9544bab5c1022ecba
labexams <86876820+labexams@users.noreply.github.com>
2022-11-26 11:16:25 +0530

    Update MainActivity.java

--- Intent/MainActivity.java
+++ Intent/MainActivity.java
@@ -1,39 +1,32 @@
-package com.example.form;
+package com.example.intent1;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.app.Activity;
-import android.content.Intent;
-import android.view.Menu;
-import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity{
+public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final EditText et = (EditText) findViewById(R.id.editText1);
-        final EditText et1 = (EditText) findViewById(R.id.editText2);
-        Button send = (Button) findViewById(R.id.button1);
-        send.setOnClickListener(new OnClickListener(){
+        final EditText et2 = (EditText)findViewById(R.id.editText2) ;
+        Button send = (Button) findViewById(R.id.button);
+        send.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View arg0) {
+            public void onClick(View view) {
                 String s = et.getText().toString();
-                String s1 = et1.getText().toString();
-                Intent it = new Intent(MainActivity.this, Second.class);
-                it.putExtra("uname", s);
+                String s1 = et2.getText().toString();
+                Intent it = new Intent(MainActivity.this, second.class);
+                it.putExtra("uname" ,s);
                 it.putExtra("pwd", s1);
                 startActivity(it);
             }
         });
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 }
-
